4b05e02d59fafe888e159936d15eec75efb48378
Denis_L <denis88lyapin@gmail.com>
2023-05-02 21:55:34 +0300

    added load_data, ilters_operations to func.py

--- src/func.py
+++ src/func.py
@@ -2,10 +2,27 @@ import os
 import json
 
 def load_data(path):
+    """
+    Загружает данные из файла с операциями
+    """
     with open(path, "r", encoding="utf-8") as file:
         data = json.load(file)
     return data
 
-file_path = os.path.join("../tmp/operations.json")
+def filters_operations(data):
+    """
+    Возвращает отфильтрованные операции
+    :param data: список всех операций
+    :return: список успешных опрераций
+    """
+    cleared_operations = []
+    for operation in data:
+        if operation.get("state") == "EXECUTED":
+            cleared_operations.append(operation)
+    return cleared_operations
 
-print(load_data(file_path))
+
+file_path = os.path.join("../tmp/operations.json")
+a = load_data(file_path)
+
+print(filters_operations(a))
