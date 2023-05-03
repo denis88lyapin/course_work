a25823bb33a317e8cda143f51e36c581a4556738
Denis_L <denis88lyapin@gmail.com>
2023-05-03 20:52:59 +0300

    new feature in load_data and filters_operations to func.py

--- src/func.py
+++ src/func.py
@@ -1,28 +1,39 @@
 import os
 import json
+from datetime import datetime
+
 
 def load_data(path):
     """
     Загружает данные из файла с операциями
     """
-    with open(path, "r", encoding="utf-8") as file:
-        data = json.load(file)
+    if not os.path.isfile(path):
+        raise FileNotFoundError(f"Файл {path} не найден")
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+    except json.JSONDecodeError:
+        raise ValueError((f"Файл {path} содержит неверный формат данных"))
     return data
+
+
 
 def filters_operations(data):
     """
-    Возвращает отфильтрованные операции
+    Возвращает отфильтрованные операции, убирает операции без необходимых для вывода данных
     :param data: список всех операций
-    :return: список успешных опрераций
+    :return: список отфильтрованных опрераций, отсортированный по дате и времени
     """
     cleared_operations = []
     for operation in data:
-        if operation.get("state") == "EXECUTED":
-            cleared_operations.append(operation)
+        if all(key in operation and operation[key] for key in ["state", "date", "description", "from", "to", "operationAmount"]):
+            if operation.get("state") == "EXECUTED":
+                cleared_operations.append(operation)
+    cleared_operations.sort(key=lambda x: datetime.strptime(x.get("date"), "%Y-%m-%dT%H:%M:%S.%f"))
     return cleared_operations
 
 
-file_path = os.path.join("../tmp/operations.json")
-a = load_data(file_path)
-
-print(filters_operations(a))
+# file_path = os.path.join("../tmp/operations.json")
+# a = load_data(file_path)
+#
+# print(filters_operations(a))
