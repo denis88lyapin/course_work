eeedbc2691ffc6c4498c55ee32e003d5feb7581a
Denis_L <denis88lyapin@gmail.com>
2023-05-04 23:00:58 +0300

    added code in main.py

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import func
+import os
+
+
+file_path = os.path.join("../tmp/operations.json")
+def main():
+
+    data = func.load_data(file_path)
+
+    data_filtered = func.filters_operations(data)
+
+    data_sort = func.sort_date(data_filtered)
+
+    data_format = func.data_format(data_sort)
+
+    data_work = func.adding_last_operations(data_format)
+
+    for data in data_work:
+        print(f""
+              f"{data['date']} {data['description']}\n"
+              f"{data['from']} -> {data['to']}\n"
+              f"{data['amount']} {data['currency']}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
