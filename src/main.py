40eaccd20bd80e9e65c4675c2912901195254b53
Denis_L <denis88lyapin@gmail.com>
2023-05-05 18:09:28 +0300

    docstrings

--- src/main.py
+++ src/main.py
@@ -1,20 +1,20 @@
 import func
 import os
 
-
+# файл с данными
 file_path = os.path.join("../tmp/operations.json")
 def main():
-
+    # загрузка данных из файла
     data = func.load_data(file_path)
-
+    # фильтрация данных
     data_filtered = func.filters_operations(data)
-
+    # сортировка данных
     data_sort = func.sort_date(data_filtered)
-
+    # форматирование данных
     data_format = func.data_format(data_sort)
-
+    # выдача последних операций
     data_work = func.adding_last_operations(data_format)
-
+    # вывод операций
     for data in data_work:
         print(f""
               f"{data['date']} {data['description']}\n"
