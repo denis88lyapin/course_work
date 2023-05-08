3921cf8981bbd152db881ddca0f0d7796c82274a
Denis_L <denis88lyapin@gmail.com>
2023-05-08 13:28:51 +0300

    added test_filters_operations_executed_operations test_filters_operations.py

--- tests/tests_func/test_filters_operations.py
+++ tests/tests_func/test_filters_operations.py
@@ -1,2 +1,58 @@
 from src.func import filters_operations
+import pytest
 
+
+@pytest.fixture
+def data():
+    """Тестовые данные"""
+    return [{
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    },
+        {
+            "id": 615064591,
+            "state": "CANCELED",
+            "date": "2018-10-14T08:21:33.419441",
+            "operationAmount": {
+                "amount": "77751.04",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод с карты на счет",
+            "from": "Maestro 3928549031574026",
+            "to": "Счет 84163357546688983493"
+        },
+        {}]
+
+
+def test_filters_operations_executed_operations(data):
+    """Проверка функции фильтрации"""
+    expected_result = [{
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 1596837868705199",
+        "to": "Счет 64686473678894779589"
+    }]
+    assert filters_operations(data) == expected_result
